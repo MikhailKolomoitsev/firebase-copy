786839a0997bd7bbfa4d631c83f6c78ef130d4b0
Mihon <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-03-09 10:54:51 +0000

    Updates

---
+++ precache-manifest.3ec665dd45458c3b91fb61983ee853a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12efe49367ae6f68d45e94b794664b85",
+    "url": "/firebase-copy/index.html"
+  },
+  {
+    "revision": "dac7c14faf2b66551379",
+    "url": "/firebase-copy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dac7c14faf2b66551379",
+    "url": "/firebase-copy/static/js/2.b904ede5.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/firebase-copy/static/js/2.b904ede5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a7aae229994c04008c",
+    "url": "/firebase-copy/static/js/main.cdc9a7c5.chunk.js"
+  },
+  {
+    "revision": "9f4a325a82c0d5ff65cc",
+    "url": "/firebase-copy/static/js/runtime-main.736d31d3.js"
+  }
+]);
